3e162be0f98cc6c4d507d30d5196edc6cc8cb928
Pawan Gurung <pawan.grgn@gmail.com>
2024-12-29 20:28:04 +0545

    collision function

--- jscript/ballCollisionOOP.js
+++ jscript/ballCollisionOOP.js
@@ -23,7 +23,6 @@ class Ball{
 
         this.container.appendChild(this.element);
 
-
     }
 
    static randomColor(){
@@ -38,7 +37,6 @@ class Ball{
 
    move(){
 
-    console.log('Ball is moving:', this.element.style.top, this.element.style.left);
     let currentTop = parseFloat(this.element.style.top) || 0;
     let currentLeft = parseFloat(this.element.style.left) || 0;
     const ballHeight = parseFloat(this.element.style.height);
@@ -100,12 +98,39 @@ function createBall(diameter) {
     balls.push(ball);
 }
 
+function areColliding(ballA, ballB) {
+    const ax = parseFloat(ballA.element.style.left) + parseFloat(ballA.element.style.width) / 2;
+    const ay = parseFloat(ballA.element.style.top) + parseFloat(ballA.element.style.height) / 2;
+    const bx = parseFloat(ballB.element.style.left) + parseFloat(ballB.element.style.width) / 2;
+    const by = parseFloat(ballB.element.style.top) + parseFloat(ballB.element.style.height) / 2;
+    
+    const distance = Math.sqrt((ax - bx) ** 2 + (ay - by) ** 2);
+    const radiusA = parseFloat(ballA.element.style.width) / 2;
+    const radiusB = parseFloat(ballB.element.style.width) / 2;
+    
+    return distance < radiusA + radiusB;
+}
+
 function moveBalls() {
     console.log('Ball is moving:');
     balls.forEach(function(ball){
         ball.move()
-    })
-}
+
+        for (let i = 0; i < balls.length; i++) {
+            for (let j = i + 1; j < balls.length; j++) {
+                if (areColliding(balls[i], balls[j])) {
+                    [balls[i].direction, balls[j].direction] = [balls[j].direction, balls[i].direction];
+                    balls[i].down = !balls[i].down;
+                    balls[j].down = !balls[j].down;
+                    balls[i].right = !balls[i].right;
+                    balls[j].right = !balls[j].right;
+                    }
+                }
+            }
+        }
+        
+   ) }
+
 
 createBall(19);
 createBall(20);
@@ -114,4 +139,25 @@ createBall(28);
 createBall(15);
 createBall(17);
 
-setInterval(moveBalls, 20);
+
+setInterval(moveBalls,30);
+
+// function collison(){
+//     console.log(balls);
+//     console.log(balls[0].element.style.left);
+//     let temp = '';
+
+//     for (let i=0;i<balls.length;i++){
+//         for(let j=i+1;j<balls.length;j++){
+//             if (balls[i].element.style.left===balls[j].element.style.left && balls[i].element.style.top===balls[j].element.style.top){
+//                 temp=balls[i].direction;
+//                 balls[i].direction=balls[j].direction;
+//                 balls[j].direction=temp;
+//             }
+//         }
+//     }
+
+//     //if ballA.element.style.left === ballB.element.style.left and ballA.element.style.top === ballB.element.style.top{
+//     //ballA.direction=ballB.direction}
+
+// }
